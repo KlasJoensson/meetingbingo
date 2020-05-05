a0495cebd229f9898b501472f9492fe702655a9e
klasjonsson <klasjonsson@192.168.0.5>
2020-05-05 07:58:21 +0200

    Adds a menu

--- src/com/xlent/meetingbingo/MeetingBingo.java
+++ src/com/xlent/meetingbingo/MeetingBingo.java
@@ -5,11 +5,20 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MeetingBingo extends Application {
+	
+	private VBox mainPlane;
+	
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -38,12 +47,87 @@ public class MeetingBingo extends Application {
 		primaryStage.setTitle("Mötes bingo");
 		
 		GamePad gp = new GamePad();
+		GridPane gamePlane = gp.getGamePad();
 		
-		GridPane mainPlane = gp.getGamePad();
+		VBox menu = new VBox(getMenuBar());
+		
+		mainPlane = new VBox(menu);
+		mainPlane.getChildren().add(gamePlane);
+		
 		Scene scene = new Scene(mainPlane, 500, 500);
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
 	}
 
+	/**
+	 * Creates the menu bar.
+	 * 
+	 * @return A populated menu bar
+	 */
+	private MenuBar getMenuBar() {
+		Menu fileMenu = new Menu("Arkiv");
+		
+		
+		Menu langMenu = new Menu("Nytt spel");
+		MenuItem langEn = new MenuItem("Engelska");
+		langEn.setOnAction(enGameEventHandler);
+		langMenu.getItems().add(langEn);
+		MenuItem langSv = new MenuItem("Svenska");
+		langSv.setOnAction(svGameEventHandler);
+		langMenu.getItems().add(langSv);
+		fileMenu.getItems().add(langMenu);
+		
+		MenuItem quitGame = new MenuItem("Avsluta");
+		quitGame.setOnAction(quitEventHandler);
+		fileMenu.getItems().add(quitGame);
+		
+		
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().add(fileMenu);
+		
+		return menuBar;
+	}
+	
+	/**
+	 * Handles clicks on the exit menu item.
+	 */
+	private EventHandler<ActionEvent> quitEventHandler = new EventHandler<ActionEvent>() {
+
+		@Override
+		public void handle(ActionEvent event) {
+			System.exit(0);
+		}
+	};
+	
+	/**
+	 * Handles clicks on the new game Swedish menu item.
+	 */
+	private EventHandler<ActionEvent> svGameEventHandler = new EventHandler<ActionEvent>() {
+
+		@Override
+		public void handle(ActionEvent event) {
+			GamePad gp = new GamePad();
+			GridPane gamePlane = gp.getGamePad();
+			mainPlane.getChildren().remove(1);
+			mainPlane.getChildren().add(gamePlane);
+			
+
+		}
+	};
+	
+	/**
+	 * Handles clicks on the new game English menu item.
+	 */
+	private EventHandler<ActionEvent> enGameEventHandler = new EventHandler<ActionEvent>() {
+
+		@Override
+		public void handle(ActionEvent event) {
+			GamePad gp = new GamePad("en");
+			GridPane gamePlane = gp.getGamePad();
+			mainPlane.getChildren().remove(1);
+			mainPlane.getChildren().add(gamePlane);
+		}
+	};
 }
